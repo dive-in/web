c99f159b4d4398e91fbb30cf8a35077640b7f055
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-11-15 17:12:38 +0100

    change: Move TypeORM configuration in env variables

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -1,15 +1,7 @@
 import { createConnection, Connection } from 'typeorm';
 
 const createDatabaseConnection = async (): Promise<Connection> => {
-  const connection = await createConnection({
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    entities: ['entities/*.ts'],
-    migrations: ['migrations/*.ts'],
-    cli: {
-      migrationsDir: 'migration',
-    },
-  });
+  const connection = await createConnection();
 
   return connection;
 };
