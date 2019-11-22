5e948d700f36ce50c6e9615bdfadb07cdc2ff534
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-11-22 16:49:21 +0100

    feat: Setup users route for userController

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,22 @@
 import * as Koa from 'koa';
+import * as Router from '@koa/router';
 import * as logger from 'koa-logger';
+import * as bodyParser from 'koa-bodyparser';
 import * as json from 'koa-json';
 import createDatabaseConnection from './config/database';
+import userController from './controllers/user';
 
 const app = new Koa();
 
 app.use(logger());
 app.use(json());
+app.use(bodyParser());
+
+const router = new Router();
+
+router.use('/users', userController.routes(), userController.allowedMethods());
+
+app.use(router.routes());
 
 const port = process.env.PORT ?? 8080;
 
