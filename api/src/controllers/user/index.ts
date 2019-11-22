0d120c2b836ed586f039a121feb86851a2a64b00
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-11-22 16:49:07 +0100

    feat: Finish up logic for authenticate endpoint and use service

--- api/src/controllers/user/index.ts
+++ api/src/controllers/user/index.ts
@@ -1,46 +1,70 @@
-import * as Router from 'koa-router';
+import * as Router from '@koa/router';
 import { getManager } from 'typeorm';
 import User from '../../entities/User';
 import ResponseBody from '../../types/ResponseBody';
-import {
-  Dependencies,
-  UserControllerMiddleware,
-  AuthenticateUserRequest,
-} from './types';
+import { Dependencies, UserControllerMiddleware } from './types';
+import { validateAuthenticateUserRequest } from './validators';
+import { User as IUser } from '../../services/user/types';
+import UserService from '../../services/user';
 
 const userController = new Router<{}, Dependencies>();
 
-const injectDependencies = (): Router.IMiddleware<{}, Dependencies> => async (
+const injectDependencies = (): Router.Middleware<{}, Dependencies> => async (
   ctx,
   next
 ): Promise<void> => {
-  ctx.userRepository = getManager().getRepository(User);
+  const userRepository = getManager().getRepository(User);
+
+  ctx.userService = UserService.getInstance(userRepository);
 
   await next();
 };
 
 userController.use(injectDependencies());
 
-const authenticateUser: UserControllerMiddleware = ctx => {
-  const { accessToken } = ctx.body as AuthenticateUserRequest;
+const authenticateUser: UserControllerMiddleware = async ctx => {
+  const body = ctx.request.body as IUser;
 
-  // const { userRepository } = ctx;
+  const { userService } = ctx;
 
-  // TODO: Validate entire body
-  if (!accessToken) {
+  const hasErrors = validateAuthenticateUserRequest(body);
+
+  if (hasErrors) {
     ctx.status = 400;
 
     const response: ResponseBody<string> = {
       status: 400,
-      message: 'Required parameter `accessToken` is missing.',
+      message: 'Invalid request body',
     };
 
     ctx.body = response;
 
-    ctx.res.end();
+    return;
   }
 
-  // TODO: Handle logic of adding user if not present and generating JWT and refresh token
+  try {
+    const newUser = await userService.saveOrUpdate(body);
+
+    const token = userService.generateToken(newUser.id, body.accessToken);
+
+    ctx.status = 200;
+
+    const response: ResponseBody<string> = {
+      status: 200,
+      message: token,
+    };
+
+    ctx.body = response;
+  } catch (e) {
+    ctx.status = 500;
+
+    const response: ResponseBody<string> = {
+      status: 500,
+      message: 'An error occurred. Please try again.',
+    };
+
+    ctx.body = response;
+  }
 };
 
 userController.post('/authenticate', authenticateUser);
--- api/src/controllers/user/types.ts
+++ api/src/controllers/user/types.ts
@@ -1,13 +1,8 @@
-import { Repository } from 'typeorm';
 import { IMiddleware } from 'koa-router';
-import User from '../../entities/User';
+import UserService from '../../services/user';
 
 export interface Dependencies {
-  userRepository: Repository<User>;
+  userService: UserService;
 }
 
 export type UserControllerMiddleware = IMiddleware<{}, Dependencies>;
-
-export interface AuthenticateUserRequest {
-  accessToken: string;
-}
