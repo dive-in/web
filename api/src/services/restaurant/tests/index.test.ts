20f4d87fa2b00a14064c68c92b0a91851eca7591
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-12-03 22:21:36 +0100

    change: Mock LocationService in RestaurantService tests

--- api/src/services/restaurant/tests/index.test.ts
+++ api/src/services/restaurant/tests/index.test.ts
@@ -8,32 +8,32 @@ const mockRestaurants: Restaurant[] = [
   {
     id: 1,
     name: 'Restaurant 1',
-    latitude: 41.99224,
-    longitude: 21.405267,
+    latitude: 55,
+    longitude: 15,
     tables: [],
     logoUrl: 'logo',
   },
   {
     id: 2,
     name: 'Restaurant 2',
-    latitude: 41.991845,
-    longitude: 21.404977,
+    latitude: 43,
+    longitude: 21,
     tables: [],
     logoUrl: 'logo',
   },
   {
     id: 3,
     name: 'Restaurant 3',
-    latitude: 41.992633,
-    longitude: 21.404151,
+    latitude: 100,
+    longitude: 100,
     tables: [],
     logoUrl: 'logo',
   },
   {
     id: 4,
     name: 'Restaurant 4',
-    latitude: 41.991867,
-    longitude: 21.404572,
+    latitude: 35,
+    longitude: 25,
     tables: [],
     logoUrl: 'logo',
   },
@@ -45,10 +45,34 @@ const mockRestaurantRepository = {
   find,
 };
 
+const isDistanceLessThan: jest.Mock<
+  boolean,
+  [Coordinate, Coordinate, number]
+> = jest.fn(
+  (first, second, maxDistance) =>
+    Math.abs(first.latitude - second.latitude) +
+      Math.abs(first.longitude - second.longitude) <=
+    maxDistance
+);
+
+const calculateDistanceBetween: jest.Mock<
+  number,
+  [Coordinate, Coordinate]
+> = jest.fn(
+  (first, second) =>
+    Math.abs(first.latitude - second.latitude) +
+    Math.abs(first.longitude - second.longitude)
+);
+
+const mockLocationService: LocationService = {
+  isDistanceLessThan,
+  calculateDistanceBetween,
+};
+
 describe('RestaurantService', () => {
   const restaurantService = RestaurantService.getInstance(
     (mockRestaurantRepository as unknown) as Repository<Restaurant>,
-    LocationService.getInstance()
+    mockLocationService
   );
 
   beforeEach(() => {
@@ -62,8 +86,8 @@ describe('RestaurantService', () => {
       );
 
       const coordinate: Coordinate = {
-        latitude: 41.991867,
-        longitude: 21.404889,
+        latitude: 42,
+        longitude: 22,
       };
 
       const closestRestaurants = await restaurantService.getClosestRestaurantsTo(
@@ -83,7 +107,7 @@ describe('RestaurantService', () => {
     it('should only return 2 restaurants if there are only 2 nearby', async () => {
       const newMockRestaurants: Restaurant[] = [
         ...mockRestaurants.slice(0, 3),
-        { ...mockRestaurants[3], latitude: 41.993215, longitude: 21.408256 },
+        { ...mockRestaurants[3], latitude: 170, longitude: 170 },
       ];
 
       mockRestaurantRepository.find.mockReturnValue(newMockRestaurants);
@@ -108,9 +132,11 @@ describe('RestaurantService', () => {
   });
 
   it('should return empty array if there are no restaurants nearby', async () => {
+    mockRestaurantRepository.find.mockReturnValue(mockRestaurants);
+
     const coordinate: Coordinate = {
-      latitude: 43,
-      longitude: 24,
+      latitude: 170,
+      longitude: 170,
     };
 
     const closestRestaurants = await restaurantService.getClosestRestaurantsTo(
