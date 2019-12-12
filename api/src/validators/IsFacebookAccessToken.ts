8dd5db5a884aae9ecab9a42256e34635c7eeef95
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-12-12 11:45:10 +0100

    feat: Add custom validator for validating FB access token

---
+++ api/src/validators/IsFacebookAccessToken.ts
@@ -0,0 +1,47 @@
+import {
+  ValidatorConstraintInterface,
+  ValidatorConstraint,
+  ValidationArguments,
+  ValidationOptions,
+  registerDecorator,
+} from 'class-validator';
+import * as superAgent from 'superagent';
+
+@ValidatorConstraint({ async: true })
+class IsFacebookAccessTokenConstraint implements ValidatorConstraintInterface {
+  async validate(
+    value: any,
+    validationArguments?: ValidationArguments
+  ): Promise<boolean> {
+    console.error(value);
+    const url = `https://graph.facebook.com/debug_token?input_token=${value}&access_token=${process.env.FACEBOOK_APP_ACCESS_TOKEN}`;
+
+    try {
+      const response = await superAgent.get(url);
+      const responseBody = JSON.parse(response.text);
+
+      return responseBody.data.is_valid;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return 'Provided access token is not a valid Facebook access token';
+  }
+}
+
+export default function IsFacebookAccessToken(
+  validationOptions?: ValidationOptions
+): PropertyDecorator {
+  return function(object: Record<string, any>, propertyName: string): void {
+    registerDecorator({
+      name: 'isFacebookAccessToken',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsFacebookAccessTokenConstraint,
+    });
+  };
+}
